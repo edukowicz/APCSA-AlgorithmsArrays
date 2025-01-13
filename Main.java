0a4e1294d11e6f6048e50be424e757e6f249d067
Ryan James <ryanjames1729@gmail.com>
2025-01-13 08:29:04 -0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+// Algorithms using arrays starter    A+CR  
+class Main {
+    public static void main(String[] args) {
+      
+          double[] pollution = {0.0037, 0.0057, 0.0186, 0.0329, 0.0083, 0.0104, 0.0004, 0.0234, 0.0265, 0.0289, 0.0336, 0.0213, 0.0172, 0.0124, 0.0216, 0.016, 0.0336, 0.0228, 0.0166, 0.0088};
+  
+          for(int i = 1; i <= pollution.length; i++){
+        System.out.println("Location " + i + " " + pollution[i-1]);
+      }
+      System.out.println("\n\n");
+  
+  
+          
+          
+  
+    }
+  }
